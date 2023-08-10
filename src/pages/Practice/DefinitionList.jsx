5a09f615539e288d0eaee3ecc6978169f0d325ec
miinje <njjj00083@gmail.com>
2023-08-10 15:55:40 +0900

    조건부 렌더링 vs. 조건부 표시

--- src/pages/Practice/DefinitionList.jsx
+++ src/pages/Practice/DefinitionList.jsx
@@ -1,4 +1,3 @@
-
 // 내부에 분리된 컴포넌트 호출
 // 학습 주제별 컴포넌트
 import ConditionalDisplay from './parts/ConditionalDisplay';
@@ -36,14 +35,14 @@ function DefinitionList() {
 
   return (
     <dl className="descriptionList">
-        
-        <DisplayingData statusMessage={statusMessage} />
-        <ConditionalRendering imageType={imageType} />
-        <ConditionalDisplay />
-        <RenderingLists renderList={renderList} />
-        
-      </dl>
-  )
+      <DisplayingData hidden statusMessage={statusMessage} />
+      {/* 조건부 렌더링(rendering) vs. 조건부 표시(display) */}
+      {/* SSR (존재 혹은 존재하지 않음) vs. Client (화면에 표시 혹은 감춤) */}
+      <ConditionalRendering hidden imageType={imageType} />
+      <ConditionalDisplay isShowReactImage={isShowReactImage} />
+      <RenderingLists renderList={renderList} />
+    </dl>
+  );
 }
 
 export default DefinitionList;

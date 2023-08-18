f55746c7f7c86609bc01e03903a0bef219737c92
miinje <njjj00083@gmail.com>
2023-08-18 15:07:07 +0900

    내비게이션 활성화 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,76 @@
-import { useState } from 'react';
+import { createBrowserRouter, RouterProvider, NavLink } from 'react-router-dom';
 import RootLayout from './layout/RootLayout';
-import LearnStateAndEffects from './pages/LearnStateAndEffects';
-import ProductList from './pages/ProductList';
+
+const Nav = () => (
+  <nav>
+    <ul>
+      <li>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? 'font-black text-rose-600' : ''
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/products"
+          className={({ isActive }) =>
+            isActive ? 'font-black text-rose-600' : ''
+          }
+        >
+          Products
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            isActive ? 'font-black text-rose-600' : ''
+          }
+        >
+          Contact
+        </NavLink>
+      </li>
+    </ul>
+  </nav>
+);
+
+const Home = () => (
+  <div>
+    <Nav />
+    <h1 className="text-black text-emerald-500">Home</h1>
+  </div>
+);
+const Products = () => (
+  <div>
+    <Nav />
+    <h1 className="text-black text-emerald-500">Products</h1>
+  </div>
+);
+const Contact = () => (
+  <div>
+    <Nav />
+    <h1 className="text-black text-emerald-500">Contact</h1>
+  </div>
+);
+
+const router = createBrowserRouter([
+  // Router Object
+  // 경로(path), 요소(element ← <Component />)
+  { path: '/', element: <Home /> },
+  { path: '/products', element: <Products /> },
+  { path: '/contact', element: <Contact /> },
+]);
+
+console.log(router);
 
 function App() {
-  const [isVisible] = useState(true);
-
   return (
     <div className="App">
-      <RootLayout>
-        <LearnStateAndEffects />
-        {isVisible && <ProductList />}
-      </RootLayout>
+      <RouterProvider router={router} />
     </div>
   );
 }

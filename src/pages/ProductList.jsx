b2628352eae1f4215ce4e3820eb8502917ba6e0e
miinje <njjj00083@gmail.com>
2023-08-18 13:19:06 +0900

    로직 재사용을 목적으로 커스텀 훅

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,83 +1,35 @@
-import { getPbImageURL, numberWithComma } from '@/utils';
-import { useEffect, useState } from 'react';
+import Spinner from '@/components/Spinner';
+import useFetchData from '@/hooks/useFetchData';
+import ProductItem from './ProductItem';
 
 const PB_PRODUCTS_ENDPOINT = `
   http://127.0.0.1:8090/api/collections/products/records
 `;
 
 function ProductList() {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null); // null | Error
+  const { data, isLoading, error } = useFetchData(PB_PRODUCTS_ENDPOINT);
 
-  useEffect(() => {
-    // 중단(abort) 컨트롤러(controller) 생성
-    const controller = new AbortController();
+  // 로딩 중인 경우 화면
+  if (isLoading) {
+    return <Spinner size={160} title="데이터 가져오는 중이에요." />;
+  }
 
-    setIsLoading(true);
-
-    async function fetchProducts() {
-      try {
-        const response = await fetch(PB_PRODUCTS_ENDPOINT, {
-          signal: controller.signal,
-        });
-        const responseData = await response.json();
-        console.log(responseData);
-        setData(responseData);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    fetchProducts();
-
-    // StrictMode(x2, detect impure function component)
-    // mount(1, 요청 1) → unmount (취소 1) → mount(2, 요청 1) -> 응답 1
-    return () => {
-      controller.abort();
-    }
-  }, []);
-
-  // javascript statement
-  // if (data) {
-  //   if ('items' in data) {
-  //     if (Array.isArray(data.items)) {
-  //       return data.items.map(item => (
-  //         <ProductItem key={item.id} item={item} />
-  //       ))
-  //     }
-  //   }
-  // }
+  // 오류가 발생한 경우 화면
+  if (error) {
+    return (
+      <div role="alert">
+        <h2>{error.type}</h2>
+        <p>{error.message}</p>
+      </div>
+    );
+  }
 
   return (
     <ul className="grid grid-cols-3 m-10">
-      {/* javascript expression */}
       {data &&
         data.items &&
         data.items?.map((item) => <ProductItem key={item.id} item={item} />)}
     </ul>
-  );
-}
-
-function ProductItem({ item }) {
-  return (
-    <li>
-      <figure className="flex flex-col items-start">
-        <img
-          src={getPbImageURL(item, 'photo')}
-          className="h-96 w-auto"
-          alt=""
-        />
-        <figcaption className="flex flex-col">
-          <span className="title">
-            {item.title} [ {item.color} ]
-          </span>
-          <span className="price">KRW {numberWithComma(item.price)}</span>
-        </figcaption>
-      </figure>
-    </li>
   );
 }
 

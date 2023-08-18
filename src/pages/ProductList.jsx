aaa86cfda14caad92e530a656c30579d0e374686
miinje <njjj00083@gmail.com>
2023-08-18 11:46:30 +0900

    useEffect 훅 그리고 통신 중단(AbortController)

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,5 +1,5 @@
-import { getPbImageURL, numberWithComma } from "@/utils";
-import { useEffect, useState } from "react"
+import { getPbImageURL, numberWithComma } from '@/utils';
+import { useEffect, useState } from 'react';
 
 const PB_PRODUCTS_ENDPOINT = `
   http://127.0.0.1:8090/api/collections/products/records
@@ -11,11 +11,16 @@ function ProductList() {
   const [error, setError] = useState(null); // null | Error
 
   useEffect(() => {
+    // 중단(abort) 컨트롤러(controller) 생성
+    const controller = new AbortController();
+
     setIsLoading(true);
 
     async function fetchProducts() {
       try {
-        const response = await fetch(PB_PRODUCTS_ENDPOINT)
+        const response = await fetch(PB_PRODUCTS_ENDPOINT, {
+          signal: controller.signal,
+        });
         const responseData = await response.json();
         console.log(responseData);
         setData(responseData);
@@ -27,6 +32,12 @@ function ProductList() {
     }
 
     fetchProducts();
+
+    // StrictMode(x2, detect impure function component)
+    // mount(1, 요청 1) → unmount (취소 1) → mount(2, 요청 1) -> 응답 1
+    return () => {
+      controller.abort();
+    }
   }, []);
 
   // javascript statement
@@ -42,20 +53,23 @@ function ProductList() {
 
   return (
     <ul className="grid grid-cols-3 m-10">
-      { data && data.items && data.items?.map(item => (
-        <ProductItem key={item.id} item={item} />
-      )) }
+      {/* javascript expression */}
+      {data &&
+        data.items &&
+        data.items?.map((item) => <ProductItem key={item.id} item={item} />)}
     </ul>
-  )
+  );
 }
 
-function ProductItem({ item /* { id, created, title, color, price, photo } */ }) {
-  // console.log(item);
-
+function ProductItem({ item }) {
   return (
     <li>
       <figure className="flex flex-col items-start">
-        <img src={getPbImageURL(item, 'photo')} className="h-96 w-auto" alt="" />
+        <img
+          src={getPbImageURL(item, 'photo')}
+          className="h-96 w-auto"
+          alt=""
+        />
         <figcaption className="flex flex-col">
           <span className="title">
             {item.title} [ {item.color} ]
@@ -64,7 +78,7 @@ function ProductItem({ item /* { id, created, title, color, price, photo } */ })
         </figcaption>
       </figure>
     </li>
-  )
+  );
 }
 
-export default ProductList
+export default ProductList;

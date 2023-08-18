e9ee81c3e56ce300fa7dce74e3f1e493daf3d0a5
miinje <njjj00083@gmail.com>
2023-08-18 09:16:21 +0900

    jsx 파일 확장자 오류 수정 및 App.jsx 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import RootLayout from './layout/RootLayout';
 import LearnStateAndEffects from './pages/LearnStateAndEffects';
 
+// 데이터 가져오기 (PocketBase 서버: 백엔드 데이터베이스 솔루션)
+
+// 1. 컴포넌트에서 관리할 상태 정의
+// 2. 서버에 데이터 가져오기 오쳥/응답
+// 3. 응답된 상황(status)에 따라 뷰(view) 전환: 조건부 렌더링
+
 function App() {
   return (
     <div className="App">

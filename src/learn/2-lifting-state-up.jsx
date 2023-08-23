d15a285708c4475e8a4984808c9c55b839bf3a1e
miinje <njjj00083@gmail.com>
2023-08-23 13:02:44 +0900

    2 리프팅 후

--- src/learn/2-lifting-state-up.jsx
+++ src/learn/2-lifting-state-up.jsx
@@ -1,11 +1,14 @@
-
 import debounce from '@/utils/debounce';
 import { useState } from 'react';
 
 function LiftingStateUp() {
+  const [color, setColor] = useState('#999');
+  const handleChangeBgColor = (newColor) => setColor(newColor);
+
   return (
-    <div className="LiftingStateUp">
-      <Parent />
+    <div className="PassingProps">
+      <Parent color={color} onChangeColor={handleChangeBgColor} />
+      <OtherParent color={color} />
     </div>
   );
 }
@@ -14,13 +17,25 @@ export default LiftingStateUp;
 
 /* -------------------------------------------------------------------------- */
 
-export function Parent() {
-  const [color, setColor] = useState('#999');
-  const handleChangeBgColor = (newColor) => setColor(newColor);
+function OtherParent({ color }) {
+  // 나도 데이터 필요해!
+  // 단락의 배경색을 바꾸고 싶거든!
+  return (
+    <div
+      className="Parent flex justify-center items-center bg-slate-200 p-4"
+      style={{
+        backgroundColor: color,
+      }}
+    >
+      <p className="">Change Background Color</p>
+    </div>
+  );
+}
 
+export function Parent({ color, onChangeColor }) {
   return (
     <div className="Parent">
-      <Child color={color} onChangeColor={handleChangeBgColor} />
+      <Child color={color} onChangeColor={onChangeColor} />
     </div>
   );
 }
@@ -28,7 +43,9 @@ export function Parent() {
 export function Child({ color, onChangeColor }) {
   return (
     <div className="Child flex items-center justify-center gap-8">
-      <p className='text-4xl font-extralight uppercase' style={{ color }}>Child</p>
+      <p className="text-4xl font-extralight uppercase" style={{ color }}>
+        Child
+      </p>
       <input
         type="color"
         aria-label="글자 색상 변경"
@@ -38,4 +55,3 @@ export function Child({ color, onChangeColor }) {
     </div>
   );
 }
-

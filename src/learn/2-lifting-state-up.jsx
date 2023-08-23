dfe29b3d3beea592f317db9620b7e1c5f5f08bd5
miinje <njjj00083@gmail.com>
2023-08-23 11:25:44 +0900

    02 리프팅 전 코드

--- src/learn/2-lifting-state-up.jsx
+++ src/learn/2-lifting-state-up.jsx
@@ -1,5 +1,41 @@
+
+import debounce from '@/utils/debounce';
+import { useState } from 'react';
+
 function LiftingStateUp() {
-  return <></>;
+  return (
+    <div className="LiftingStateUp">
+      <Parent />
+    </div>
+  );
 }
 
 export default LiftingStateUp;
+
+/* -------------------------------------------------------------------------- */
+
+export function Parent() {
+  const [color, setColor] = useState('#999');
+  const handleChangeBgColor = (newColor) => setColor(newColor);
+
+  return (
+    <div className="Parent">
+      <Child color={color} onChangeColor={handleChangeBgColor} />
+    </div>
+  );
+}
+
+export function Child({ color, onChangeColor }) {
+  return (
+    <div className="Child flex items-center justify-center gap-8">
+      <p className='text-4xl font-extralight uppercase' style={{ color }}>Child</p>
+      <input
+        type="color"
+        aria-label="글자 색상 변경"
+        defaultValue={color}
+        onChange={debounce((e) => onChangeColor(e.target.value), 500)}
+      />
+    </div>
+  );
+}
+

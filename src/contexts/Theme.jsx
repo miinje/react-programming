9a59a850679844342344f4b03dffb6fba6da1852
miinje <njjj00083@gmail.com>
2023-08-25 02:07:51 +0900

    PropTypes 타입 검사 (pnpm lint)

--- src/contexts/Theme.jsx
+++ src/contexts/Theme.jsx
@@ -1,5 +1,5 @@
-/* eslint-disable react-refresh/only-export-components */
-import { createContext, useContext, useMemo, useReducer } from 'react';
+import { node, string } from 'prop-types';
+import { createContext, useContext, useState } from 'react';
 
 // Context 생성
 const ThemeContext = createContext();
@@ -17,69 +17,49 @@ const initialTheme = {
   },
 };
 
-// 액션 타입
-const RESET_THEME = 'RESET_THEME';
-const CHANGE_LIGHT_THEME = 'CHANGE_LIGHT_THEME';
-const SWITCH_MODE = 'SWITCH_MODE';
-const CHANGE_DARK_THEME = 'CHANGE_DARK_THEME';
-
-// 액션 크리에이터 (함수)
-export const resetTheme = () => ({
-  type: RESET_THEME,
-});
-
-export const changeLightTheme = (newLightTheme) => ({
-  type: CHANGE_LIGHT_THEME,
-  payload: newLightTheme,
-});
-
-export const changeDarkTheme = (newDarkTheme) => ({
-  type: CHANGE_LIGHT_THEME,
-  payload: newDarkTheme,
-});
-
-export const switchMode = () => ({
-  type: SWITCH_MODE,
-});
-
-// 리듀서 (순수 함수)
-const reducer = (state, action) => {
-  switch (action.type) {
-    case SWITCH_MODE:
-      return {
-        ...state,
-        currentMode: state.currentMode.includes('light') ? 'dark' : 'light',
-      };
-    case CHANGE_LIGHT_THEME:
-      return {
-        ...state,
-        light: action.payload,
-      };
-    case CHANGE_DARK_THEME:
-      return {
-        ...state,
-        dark: action.payload,
-      };
-    case RESET_THEME:
-      return initialTheme;
-    default:
-      return state;
-  }
-};
-
 // 컨텍스트 프로바이더 래퍼 컴포넌트
 function ThemeProvider({ displayName = 'ThemeContext.Provider', children }) {
-  const [theme, dispatch] = useReducer(reducer, initialTheme);
+  const [theme, setTheme] = useState(initialTheme);
 
-  // 메모이제이션(Memoization)
-  const themeValue = useMemo(() => ({ theme, dispatch }), [theme]);
+  const resetTheme = () => setTheme(initialTheme);
+
+  const changeLightTheme = (newLightTheme) =>
+    setTheme({
+      ...theme,
+      light: newLightTheme,
+    });
+
+  const changeDarkTheme = (newDarkTheme) =>
+    setTheme({
+      ...theme,
+      dark: newDarkTheme,
+    });
+
+  const switchMode = () =>
+    setTheme((theme) => ({
+      ...theme,
+      currentMode: theme.currentMode.includes('light') ? 'dark' : 'light',
+    }));
+
+  const value = {
+    theme,
+    resetTheme,
+    changeLightTheme,
+    changeDarkTheme,
+    switchMode,
+  };
 
   return (
-    <ThemeContext.Provider value={themeValue} displayName={displayName}>
+    <ThemeContext.Provider value={value} displayName={displayName}>
       {children}
     </ThemeContext.Provider>
   );
 }
+
+ThemeProvider.propTypes = {
+  displayName: string,
+  children: node,
+};
 
 export default ThemeProvider;
 
@@ -89,15 +69,5 @@ export function useTheme() {
   if (!contextValue) {
     throw new Error('useTheme은 ThemeProvider 내부에서만 사용 가능합니다.');
   }
-  return contextValue.theme;
-}
-
-export function useDispatch() {
-  const contextValue = useContext(ThemeContext);
-  if (!contextValue) {
-    throw new Error(
-      'useDispatch 훅은 ThemeProvider 내부에서만 사용 가능합니다.'
-    );
-  }
-  return contextValue.dispatch;
+  return contextValue;
 }

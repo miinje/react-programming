8b393b8e1b4f739dc43ab311adf38d524e538454
miinje <njjj00083@gmail.com>
2023-08-24 11:01:32 +0900

    useRef 사용 절차

--- src/learn/6-ref-2-referencing-dom.jsx
+++ src/learn/6-ref-2-referencing-dom.jsx
@@ -12,6 +12,22 @@ function RefExampleReferencingDOM() {
   );
 }
 
+// React 컴포넌트는 순수해야 한다. (렌더링 프로세스 순수해야 하기 때문에)
+// Web Animation, GSAP, jQuery와 같은 API는 명령형 프로그래밍이다. 
+// 그러므로 컴포넌트 내부에 직접 사용할 수 없다. 
+// 명령형 프로그래밍을 작성할 수 있는 구간은 2군데이다.
+// 1. useLayoutEffect 훅 (브라우저 페인팅 이전에 실행)
+// 2. Event Handler(Listener)
+// 
+// React 요소(가상)가 실제 DOM에 렌더링 된 후 요소를 참조하려면?
+// 기존의 document.getElementById, document.querySelector 사용하는 것이 권장되지 않는다.
+// 그럼 어떻게 하는게 좋냐? 공식문서 피셜 useRef 훅을 사용해 Refs 객체 생성하여 활용하라.
+// const anyRef = useRef(null);
+
+// 1. Refs 생성
+// 2. JSX 요소 ref 속성(prop)에 Refs 연결
+// 3. useLayoutEffect 훅 안에서 Refs 현재(current) 값으로 명령형 프로그래밍
+
 function Circle() {
   // DOM 요소를 참조하기 위한 Refs 생성
   const circleRef = useRef(null); // { current: null }
@@ -48,6 +64,9 @@ function Circle() {
     <figure
       role="none"
       ref={circleRef}
+      // ref={(domElement) => {
+      //   circleRef.current = domElement
+      // }}
       // onPointerEnter={handleEnter}
       // onPointerLeave={handleLeave}
       className="w-16 h-16 rounded-full bg-yellow-400"

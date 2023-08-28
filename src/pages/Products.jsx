b557c3ad3a8d03d69da2fb37ffbf17777338f2ba
miinje <njjj00083@gmail.com>
2023-08-28 09:05:44 +0900

    코드 정리
    - Products 전 코드는 잘못 퍼온 것 같음 확인 필요

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,111 +1,53 @@
-import pb from '@/api/pocketbase';
-import { cloneElement, useRef } from 'react';
+import Spinner from '@/components/Spinner';
+import useDocumentTitle from '@/hooks/useDocumentTitle';
+import useProductList from '@/hooks/useProductList';
+import { getPbImageURL, numberWithComma } from '@/utils';
 import { Helmet } from 'react-helmet-async';
-import { toast } from 'react-hot-toast';
+import { Link } from 'react-router-dom';
 
-function ProductNew() {
-  const titleRef = useRef(null);
-  const colorRef = useRef(null);
-  const priceRef = useRef(null);
-  const photoRef = useRef(null);
-  const uploadPhotoRef = useRef(null);
+function Products() {
+  useDocumentTitle('제품 목록');
+  const { isLoading, data } = useProductList();
 
-  const handleRegisterProduct = async (e) => {
-    e.preventDefault();
+  if (isLoading) {
+    return <Spinner size={160} />;
+  }
 
-    const formData = new FormData();
-    formData.append('title', titleRef.current.value);
-    formData.append('color', colorRef.current.value);
-    formData.append('price', Number(priceRef.current.value));
-    formData.append('photo', photoRef.current.files[0]);
-
-    try {
-      await pb.collection('products').create(formData);
-      toast.success('상품 등록에 성공했습니다!', {
-        position: 'top-center',
-        ariaProps: {
-          role: 'status',
-          'aria-live': 'polite',
-        },
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleDisplayUploadPhoto = (e) => {
-    const photoFile = e.target.files[0];
-    const photoUrl = URL.createObjectURL(photoFile);
-    uploadPhotoRef.current.setAttribute('src', photoUrl);
-  };
-
-  return (
-    <>
-      <Helmet>
-        <title>Register Product - ReactBird</title>
-      </Helmet>
-      <div className="flex flex-col gap-6">
-        <h2 className="text-center text-3xl">상품 등록</h2>
-        <form encType="multipart/form-data" onSubmit={handleRegisterProduct}>
-          <div>
-            <label htmlFor="title">이름</label>
-            <input
-              ref={titleRef}
-              type="text"
-              name="title"
-              id="title"
-              placeholder="상품 이름"
-            />
-          </div>
-          <div>
-            <label htmlFor="color">색상</label>
-            <input
-              ref={colorRef}
-              type="text"
-              name="color"
-              id="color"
-              placeholder="색상 이름(예: Black)"
-            />
-          </div>
-          <div>
-            <label htmlFor="price">가격</label>
-            <input
-              ref={priceRef}
-              type="text"
-              name="price"
-              id="price"
-              placeholder="가격 (예: 98000)"
-            />
-          </div>
-          <div className="relative flex flex-col gap-2 my-4">
-            <label htmlFor="photo">사진</label>
-            <input
-              ref={photoRef}
-              onChange={handleDisplayUploadPhoto}
-              className="cursor-pointer absolute w-full h-full opacity-0"
-              type="file"
-              name="photo"
-              id="photo"
-              accept="*.jpg,*.png,*.webp,*.avif"
-              multiple
-            />
-            <div className="h-[140px] bg-slate-200/80 p-2">
-              <img
-                ref={uploadPhotoRef}
-                className="h-[124px] border border-slate-400/50"
-                src="https://placehold.co/84x124?text=PHOTO"
-                alt=""
-              />
-            </div>
-          </div>
-          <div className="flex gap-2">
-            <button type="submit">등록</button>
-            <button type="reset">취소</button>
-          </div>
-        </form>
-      </div>
-    </>
-  );
+  if (data) {
+    return (
+      <>
+        <Helmet>
+          <title>Product List - ReactBird</title>
+        </Helmet>
+        <div>
+          <h1 className="text-indigo-950 text-2xl mb-5">Products</h1>
+          <ul className="grid grid-cols-3">
+            {data.items.map((item) => (
+              <li key={item.id} className="justify-self-center">
+                <Link to={`/product/edit/${item.id}`}>
+                  <figure>
+                    <img
+                      className="h-[160px] object-cover mx-auto"
+                      src={getPbImageURL(item, 'photo')}
+                      alt=""
+                    />
+                    <figcaption className="flex flex-col gap-1 items-center mt-2">
+                      <span>
+                        {item.title}({item.color})
+                      </span>
+                      <span className="font-semibold">
+                        {numberWithComma(item.price)}
+                      </span>
+                    </figcaption>
+                  </figure>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </>
+    );
+  }
 }
 
-export default ProductNew;
+export default Products;
